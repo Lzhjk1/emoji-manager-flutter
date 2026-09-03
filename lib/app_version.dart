7dd79aa63009fd02c69b66768a71e47feea66450
Roasal <everspace@qq.com>
2026-09-03 15:33:42 +0800

    chore: 版本号升至 0.0.3-rc7

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -2,4 +2,4 @@
 ///
 /// 发布安装包时无需手动同步: build-and-pack.ps1 会把这里的版本号
 /// 自动写入 installer/emoji_manager.iss 的 `#define MyAppVersion`。
-const String appVersion = '0.0.3-rc6';
+const String appVersion = '0.0.3-rc7';
